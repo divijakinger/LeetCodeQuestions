9db31f244f1e98d6ac2b1fa8caf633c9d4a1c6af
Divija Kinger <79622058+divijakinger@users.noreply.github.com>
2022-01-31 07:11:51 +0530

    Time: 0 ms (100.00%), Space: 42.9 MB (5.57%) - LeetHub

---
+++ 162-find-peak-element/162-find-peak-element.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int findPeakElement(int[] arr) {
+        int start = 0;
+        int end = arr.length-1;
+
+        while (start < end){
+            int mid = (start + (end - start)/2);
+            if (arr[mid] > arr[mid+1]){
+                // you are in the decreasing part of the array
+                // this may be the ans but look at left
+                // this is why end!=mid - 1
+                end = mid;
+            } else {
+                // you are in the ascending part of the array
+                start = mid + 1;
+            }
+        }
+        // in the end start == end pointing to the largest element in the array
+        return start;
+    }
+}
