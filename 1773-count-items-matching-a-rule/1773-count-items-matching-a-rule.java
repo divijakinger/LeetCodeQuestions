5a98773b4d9f3b410872a84918eedc5e139ef28d
Divija Kinger <79622058+divijakinger@users.noreply.github.com>
2022-01-31 11:31:44 +0530

    Time: 4 ms (69.00%), Space: 57.5 MB (5.05%) - LeetHub

--- 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.java
+++ 1773-count-items-matching-a-rule/1773-count-items-matching-a-rule.java
@@ -2,18 +2,12 @@ class Solution {
     public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
         int column = 0;
         int ans = 0;
-        if (ruleKey.equals("color")) {
-            column = 1;
-        } else if (ruleKey.equals("name")){
-            column = 2;
-        } else {
-            column = 0;
-        }
-        
-        for (int row=0;row<items.size();row++){
-            if (items.get(row).get(column).equals(ruleValue)){
-                ans++;
-            }
+        if (ruleKey.equals("type")) column = 0;
+        else if (ruleKey.equals("color")) column = 1;
+        else column = 2;
+
+        for (List<String> item : items) {
+            if (ruleValue.equals(item.get(column))) ans++;
         }
         return ans;
     }
