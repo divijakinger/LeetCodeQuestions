be61666ee64084629d9b02cb6cb2ee774a308587
Divija Kinger <79622058+divijakinger@users.noreply.github.com>
2022-02-03 07:26:49 +0530

    Time: 14 ms (37.91%), Space: 75.7 MB (15.21%) - LeetHub

---
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+        //check, swap, move
+        int i = 0;
+
+        while (i < nums.length){
+            
+            if (nums[i] != i+1){
+                int correct = nums[i]-1;
+                if ( nums[i] != nums[correct]){
+                    swapArray(nums,i,correct);
+                } else {
+                    return nums[i];
+                }
+            } else {
+                i++;
+            }
+        }
+        
+        return -1;
+
+    }
+
+    void swapArray(int[] arr,int first,int second){
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
