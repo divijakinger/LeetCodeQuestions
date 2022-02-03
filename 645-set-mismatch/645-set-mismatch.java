51da91c891d13771fbbe714387ebdd4538716261
Divija Kinger <79622058+divijakinger@users.noreply.github.com>
2022-02-03 08:01:07 +0530

    Time: 27 ms (8.46%), Space: 55.4 MB (5.15%) - LeetHub

--- 645-set-mismatch/645-set-mismatch.java
+++ 645-set-mismatch/645-set-mismatch.java
@@ -1,7 +1,6 @@
 class Solution {
     public int[] findErrorNums(int[] arr) {
        int i = 0;
-        int[] ans = new int[2];
 
         while (i < arr.length){
             int correct = arr[i]-1;
@@ -15,12 +14,11 @@ class Solution {
 
         for (int index = 0; index < arr.length; index++) {
             if (arr[index] != index+1) {
-                ans[1] = index+1;
-                ans[0] = arr[index];
+                return new int[] {arr[index],index+1};
             }
         }
 
-        return ans;
+         return new int[] {-1,-1};
     }
     
     void swapArray(int[] arr,int first,int second){
