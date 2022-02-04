c95efc6bee075d0d298fb9f98446684eee0eb4c7
Divija Kinger <79622058+divijakinger@users.noreply.github.com>
2022-02-04 08:23:59 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (5.35%) - LeetHub

---
+++ 367-valid-perfect-square/367-valid-perfect-square.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        long start = 0;
+        long end = num/2;
+        
+        if (num==1 || num == 0){
+            return true;
+        }
+        while (start<=end){
+            long mid = start + (end-start)/2;
+            if (mid*mid == num){
+                return true;
+            } else if (mid*mid < num){
+                start = mid+1;
+            } else{
+                end = mid-1;
+            }
+        }
+        
+        return false;
+    }
+}
