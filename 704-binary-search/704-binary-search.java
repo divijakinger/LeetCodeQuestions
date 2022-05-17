9eac471318f04617afb12e6655d904b316afb702
Divija Kinger <79622058+divijakinger@users.noreply.github.com>
2022-05-17 08:30:59 +0530

    Time: 0 ms (100.00%), Space: 42.7 MB (95.81%) - LeetHub

---
+++ 704-binary-search/704-binary-search.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int search(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length-1;
+
+        while (start <= end){
+
+            //find the middle element
+            //int mid = (start + end)/2;
+            //there might be a possibility that start + end will give you an error thus
+            int mid = (start + (end - start)/2);
+            if (target < arr[mid]){
+                end = mid-1;
+            }else if (target > arr[mid]){
+                start = mid+1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
