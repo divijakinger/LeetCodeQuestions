9fc136f543d8c40c524dc1dddccd5e5e7f4d207d
Divija Kinger <79622058+divijakinger@users.noreply.github.com>
2022-05-21 08:19:22 +0530

    Time: 44 ms (35.92%), Space: 77 MB (45.88%) - LeetHub

---
+++ 2274-maximum-consecutive-floors-without-special-floors/2274-maximum-consecutive-floors-without-special-floors.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+import java.util.Collections;
+class Solution {
+    public int maxConsecutive(int bottom, int top, int[] special) {
+        int[] max = new int[special.length+1];
+        Arrays.sort(special);
+        for (int i = 0;i<special.length;i++){
+            if (i==0){
+                max[i] = special[i]-bottom;
+            }
+            else {
+                max[i] = special[i]-special[i-1]-1;
+            }
+        }
+        max[special.length] = top - special[special.length-1];
+        
+        int maximum = max[0];
+        for (int ele : max){
+            if (ele >  maximum){
+                maximum = ele;
+            }
+        }
+        
+        if (maximum==1){
+            return 0;
+        } else {
+            return maximum;
+        }
+    }
+}
