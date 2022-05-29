834a801429c87195c78f7e902d159813a1bfc69f
Divija Kinger <79622058+divijakinger@users.noreply.github.com>
2022-05-29 09:45:11 +0530

    Time: 1 ms (100.00%), Space: 42.1 MB (50.00%) - LeetHub

--- 2287-rearrange-characters-to-make-target-string/2287-rearrange-characters-to-make-target-string.java
+++ 2287-rearrange-characters-to-make-target-string/2287-rearrange-characters-to-make-target-string.java
@@ -1,19 +1,19 @@
 class Solution {
     public int rearrangeCharacters(String s, String target) {
-        int[] from = new int[26];
+        int[] first = new int[26];
         for (char c : s.toCharArray()) {
-            from[c - 'a']++;
+            first[c - 'a']++;
         }
 
-        int[] to = new int[26];
+        int[] second = new int[26];
         for (char c : target.toCharArray()) {
-            to[c - 'a']++;
+            second[c - 'a']++;
         }
 
         int result = s.length();
         for (int i = 0; i < 26; i++) {
-            if (to[i] != 0) {
-                result = Math.min(result, from[i] / to[i]);
+            if (second[i] != 0) {
+                result = Math.min(result, first[i] / second[i]);
             }
         }
     return result;
