7058105e3a45dd108f24c96801889551ef060851
Divija Kinger <79622058+divijakinger@users.noreply.github.com>
2022-06-18 23:00:32 +0530

    Time: 2 ms (57.59%), Space: 42.1 MB (6.78%) - LeetHub

---
+++ 342-power-of-four/342-power-of-four.java
@@ -0,0 +1,8 @@
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        if(n==1) return true;
+        else if(n==0) return false;
+        else if(n%4!=0) return false;
+        return isPowerOfFour(n/4);
+    }
+}
