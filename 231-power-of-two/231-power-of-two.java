79b6b017dd71ccdae99847637872818920d17110
Divija Kinger <79622058+divijakinger@users.noreply.github.com>
2022-06-18 22:41:47 +0530

    Time: 2 ms (59.84%), Space: 41.7 MB (20.67%) - LeetHub

---
+++ 231-power-of-two/231-power-of-two.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if(n==1) return true;
+        else if(n==0) return false;
+        else if(n%2!=0) return false;
+        return isPowerOfTwo(n/2);
+    }
+
+}
