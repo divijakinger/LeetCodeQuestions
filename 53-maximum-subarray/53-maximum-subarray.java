faf128e1b3f6f2e2ca4cc052989ce6a6fedc3852
Divija Kinger <79622058+divijakinger@users.noreply.github.com>
2022-06-27 08:09:15 +0530

    Time: 2 ms (67.58%), Space: 82.7 MB (5.47%) - LeetHub

---
+++ 53-maximum-subarray/53-maximum-subarray.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+    int sum=nums[0], max=nums[0];
+    for (int i=1;i<nums.length;++i){
+    	sum= Math.max(sum+nums[i],nums[i]);
+    	max=Math.max(max, sum);	
+    }
+    return max;
+    }
+}
