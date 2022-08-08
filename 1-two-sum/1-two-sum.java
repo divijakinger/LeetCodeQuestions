7baddfd93364b4b9ae7d86aebc961b1a5828f11e
Divija Kinger <79622058+divijakinger@users.noreply.github.com>
2022-08-09 00:43:04 +0530

    Time: 1 ms (99.70%), Space: 42.2 MB (97.96%) - LeetHub

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -3,14 +3,10 @@ class Solution {
         
         HashMap<Integer, Integer> map = new HashMap<>();
         for (int i=0;i<nums.length;i++){
-            map.put(nums[i],i);
-        }
-        
-        for (int j=0;j<nums.length;j++){
-            Integer comp = map.get(target-nums[j]);
-            if (comp!=null && comp!=j){
-                return new int[]{comp,j};
+            if (map.containsKey(target-nums[i])){
+                return new int[]{i,map.get(target-nums[i])};
             }
+            map.put(nums[i],i);
         }
         return new int[]{-1,-1};
     }
